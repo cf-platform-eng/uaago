68e2498aeb2c5c9536a386229ed53a2d0485ec93
Michael William Boldt <mike.boldt@gmail.com>
2017-08-10 14:22:13 -0500

    Add main.

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"code.cloudfoundry.org/lager"
+	"encoding/json"
+	"fmt"
+	"github.com/cf-platform-eng/uaago/management"
+	"github.com/cf-platform-eng/uaago/cli"
+	"github.com/cf-platform-eng/uaago/uaago"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	os.Exit(run(os.Args))
+}
+
+func run(args []string) int {
+	// constructre *real* version of things and kick off flow
+	if len(args[1:]) != 4 {
+		fmt.Fprintf(os.Stderr, "Usage %s [URL] [USERNAME] [PASS] [PATH TO USER FILE]\n", args[0])
+		return 1
+	}
+
+	file, err := ioutil.ReadFile(args[4])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "File error: %s\n", err.Error())
+		return 1
+	}
+
+	users := []cli.User{}
+	err = json.Unmarshal(file, &users)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error unmarshaling json file: %s\n", err.Error())
+	}
+
+	tokenFetcher := uaago.NewUAATokenFetcher(args[1], args[2], args[3], true)
+
+	logger := lager.NewLogger("")
+	registrar, err := management.NewUaaRegistrar(args[1], tokenFetcher, true, logger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating new UAA registrar: %s\n", err.Error())
+	}
+
+	err = cli.RegisterUsers(registrar, users)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error registering users: %s\n", err.Error())
+	}
+
+	return 0
+}
